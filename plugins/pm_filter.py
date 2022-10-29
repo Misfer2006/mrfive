02e7155af09418d43b8b8dc74efa06b4ceeb6117
Hansaka Anuhas <116565970+HansakaAnuhas@users.noreply.github.com>
2022-10-29 11:31:18 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -635,11 +635,8 @@ async def auto_filter(client, msg, spoll=False):
         req = message.from_user.id if message.from_user else 0
         btn.append(
             [InlineKeyboardButton(text=f"🗓 1/{math.ceil(int(total_results) / 10)}", callback_data="pages"),
-             InlineKeyboardButton(text="NEXT ⏩", callback_data=f"next_{req}_{key}_{offset}")])
-        elif query.data == 'info':
-           await query.answer("your message", show_alert=True)
-        btn.append(
-            [InlineKeyboardButton("download", callback_data="info")])
+             InlineKeyboardButton(text="NEXT ⏩", callback_data=f"next_{req}_{key}_{offset}")]
+        )
     else:
         btn.append(
             [InlineKeyboardButton(text="🗓 1/1", callback_data="pages")]
