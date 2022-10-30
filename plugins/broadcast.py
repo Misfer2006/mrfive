8b41dd01084143314a155d5ad9b58d26a0a1e278
Hansaka Anuhas <116565970+HansakaAnuhas@users.noreply.github.com>
2022-10-30 20:39:03 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -17,25 +17,16 @@ async def verupikkals(bot, message):
     start_time = time.time()
     total_chats = await db.total_chat_count()
     done = 0
-    blocked = 0
-    deleted = 0
-    failed =0
-
     success = 0
     async for chat in chats:
         pti, sh = await broadcast_messages(int(chat['id']), b_msg)
         if pti:
             success += 1
         elif pti == False:
-            if sh == "Blocked":
-                blocked+=1
-            elif sh == "Deleted":
-                deleted += 1
             elif sh == "Error":
-                failed += 1
-        done += 1
+                done += 1
         await asyncio.sleep(2)
         if not done % 20:
-            await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_chats}\nCompleted: {done} / {total_chats}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
+            await sts.edit(f"Broadcast in progress:\n\nTotal Chats {total_chats}\nCompleted: {done} / {total_chats}\nSuccess: {success}")    
     time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
-    await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_chats}\nCompleted: {done} / {total_chats}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
+    await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Chats {total_chats}\nCompleted: {done} / {total_chats}\nSuccess: {success}")
