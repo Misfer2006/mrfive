d39321ae7a909f425a2ab9dc41b5ff396b8a91b3
Hansaka Anuhas <116565970+HansakaAnuhas@users.noreply.github.com>
2022-10-30 21:03:57 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -6,34 +6,6 @@ from database.users_chats_db import db
 from info import ADMINS
 from utils import broadcast_messages
 import asyncio
-        
-@Client.on_message(filters.command("groups_broadcast") & filters.user(ADMINS) & filters.reply)
-async def verupikkals(bot, message):
-    chats = await db.get_all_chats()
-    b_msg = message.reply_to_message
-    sts = await message.reply_text(
-        text='Broadcasting your messages...'
-    )
-    start_time = time.time()
-    total_chats = await db.total_chat_count()
-    done = 0
-    failed =0
-
-    success = 0
-    async for chat in chats:
-        pti, sh = await broadcast_messages(int(chat['id']), b_msg)
-        if pti:
-            success += 1
-        elif pti == False:
-            if sh == "Error":
-                failed += 1
-        done += 1
-        await asyncio.sleep(2)
-        if not done % 20:
-            await sts.edit(f"Groups Broadcast in progress:\n\nTotal Groups {total_chats}\nCompleted: {done} / {total_chats}\nSuccess: {success}")    
-    time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
-    await sts.edit(f"Groups Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Groups {total_chats}\nCompleted: {done} / {total_chats}\nSuccess: {success}")
-
 
 @Client.on_message(filters.command("users_broadcast") & filters.user(ADMINS) & filters.reply)
 async def verupikkals(bot, message):
@@ -67,3 +39,31 @@ async def verupikkals(bot, message):
             await sts.edit(f"Users Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
     time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
     await sts.edit(f"Users Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
+
+
+@Client.on_message(filters.command("group_broadcast") & filters.user(ADMINS) & filters.reply)
+async def verupikkals(bot, message):
+    chats = await db.get_all_chats()
+    b_msg = message.reply_to_message
+    sts = await message.reply_text(
+        text='Broadcasting your messages...'
+    )
+    start_time = time.time()
+    total_chats = await db.total_chat_count()
+    done = 0
+    failed =0
+
+    success = 0
+    async for chat in chats:
+        pti, sh = await broadcast_messages(int(chat['id']), b_msg)
+        if pti:
+            success += 1
+        elif pti == False:
+            if sh == "Error":
+                failed += 1
+        done += 1
+        await asyncio.sleep(2)
+        if not done % 20:
+            await sts.edit(f"Groups Broadcast in progress:\n\nTotal Groups {total_chats}\nCompleted: {done} / {total_chats}\nSuccess: {success}")    
+    time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
+    await sts.edit(f"Groups Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Groups {total_chats}\nCompleted: {done} / {total_chats}\nSuccess: {success}")
