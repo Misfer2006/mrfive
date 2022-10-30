4ceb1b15ebc4becc8fc2b8fe5c98433d6653735f
Hansaka Anuhas <116565970+HansakaAnuhas@users.noreply.github.com>
2022-10-30 19:45:56 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -58,7 +58,7 @@ async def next_page(bot, query):
 
     if not files:
         return
-    settings = await get_settings(query.message.chat.id)
+    settings = await get_settings(query.from_user.id)
     if settings['button']:
         btn = [
             [
