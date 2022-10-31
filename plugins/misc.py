d4cbf95bc852e89e6bd702721310c11100be7f6c
Hansaka Anuhas <116565970+HansakaAnuhas@users.noreply.github.com>
2022-10-31 19:42:58 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -14,9 +14,9 @@ async def showid(client, message):
     if chat_type == enums.ChatType.PRIVATE:
         user_id = message.chat.id
         first = message.from_user.first_name
-        last = message.from_user.last_name or ""
+        last = message.from_user.last_name or "None"
         username = message.from_user.username
-        dc_id = message.from_user.dc_id or ""
+        dc_id = message.from_user.dc_id or "None"
         await message.reply_text(
             f"<b>★ First Name:</b> {first}\n<b>★ Last Name:</b> {last}\n<b>★ Username:</b> @{username}\n<b>★ Telegram ID:</b> <code>{user_id}</code>\n<b>★ Data Centre:</b> <code>{dc_id}</code>",
             quote=True
