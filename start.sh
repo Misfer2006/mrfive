1bb8a27d9eed8b716af3dfe992bca907cebe9ffb
Hansaka Anuhas <116565970+HansakaAnuhas@users.noreply.github.com>
2022-10-31 21:05:11 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/HansakaAnuhas/FilterBot.git /FilterBot
+  git clone https://github.com/Misfer2006/mrfive.git /mrfive
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /FilterBot
+  git clone $UPSTREAM_REPO /mrfive
 fi
-cd /FilterBot
+cd /mrfive
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
